36ade33c61eeb9ee32a249f719d92e31bde7969b
Viggen <koriremanuel0@gmail.com>
2024-08-16 03:16:51 +0300

    React Hooks

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
-
-
   return (
     <div className="app">
       <NavBar />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,17 @@
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
-
   return (
     <nav>
-      <NavLink
-        to="/"
-        className="nav-link" 
-      >
+      <NavLink to="/" className="nav-link">
         Home
       </NavLink>
-      <NavLink
-        to="/about"
-        className="nav-link"
-      >
+      <NavLink to="/about" className="nav-link">
         About
+      </NavLink>
+      <NavLink to="/login" className="nav-link">
+        Login
       </NavLink>
     </nav>
   );
